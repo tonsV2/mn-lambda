f3b355aab45cb80735dfe29f66dbcc9b7afc1969
sj <sebastianthegreatful@gmail.com>
2020-11-21 12:54:38 +0700

    Api Gateway valid response

--- src/main/kotlin/dk/fitfit/BookRequestHandler.kt
+++ src/main/kotlin/dk/fitfit/BookRequestHandler.kt
@@ -1,19 +1,16 @@
 package dk.fitfit
+
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.function.aws.MicronautRequestHandler
-import java.util.UUID
 
 @Introspected
-class BookRequestHandler : MicronautRequestHandler<Book?, BookSaved?>() {
-
-    override fun execute(input: Book?): BookSaved? {
-        return if (input != null) {
-            val bookSaved = BookSaved()
-            bookSaved.name = input.name
-            bookSaved.isbn = UUID.randomUUID().toString()
-            return bookSaved
-        } else {
-            null
-        }
+class BookRequestHandler : MicronautRequestHandler<Book?, Map<String, Any>>() {
+    override fun execute(input: Book?): Map<String, Any> {
+        return validApiGatewayResponse("Hello from Lambda through Api Gateway!")
     }
 }
+
+fun validApiGatewayResponse(body: String, statusCode: Int = 200): Map<String, Any> = mapOf(
+        "statusCode" to statusCode,
+        "headers" to mapOf("Content-Type" to "application/json"),
+        "body" to body)
